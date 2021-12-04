8939d26aa9d0feeff01ed8f75e413ef5880e48ed
yadavdeepali <95540624+yadavdeepali@users.noreply.github.com>
2021-12-04 22:21:05 +0530

    Create ques8.py

---
+++ ques8.py
@@ -0,0 +1,11 @@
+a= int(input("enter the first age"))
+b= int(input("enter the second age"))
+c= int(input("enter the thied age"))
+if a>b and a>c:
+        print("a is greater")
+elif b>a and b>c:
+        print("b is greater") 
+elif c>a and c>b:
+        print("c is greater")
+else:
+        print("both aer equal")                      
