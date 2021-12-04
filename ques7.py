5f6c263d556912aba24f6a9eb2ad8576ee999d1b
yadavdeepali <95540624+yadavdeepali@users.noreply.github.com>
2021-12-04 22:20:04 +0530

    Create ques7.py

---
+++ ques7.py
@@ -0,0 +1,8 @@
+bed=int (input("enter the bad"))
+girl=int (input("enter the girl"))
+if bed> girl:
+        print("bed is empty")
+elif bed<girl:
+        print("bad is not there")  
+else:
+        print("nothing")              
