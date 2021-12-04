f9cf2a47afd74774a8dc5521dadf08e5d0a5fcf8
yadavdeepali <95540624+yadavdeepali@users.noreply.github.com>
2021-12-04 22:21:53 +0530

    Create ques9.py

---
+++ ques9.py
@@ -0,0 +1,12 @@
+user1= int(input("enter the number1="))
+user2= int(input("enter the number2="))
+user3=int(input("enter the number3="))
+if user1<user2:
+    print("a is less then b")
+elif user2>user3:
+    print("b is greater then c")
+elif user3<user1:
+    print("c is greater a")    
+else : 
+    print(" nothing")
+             
