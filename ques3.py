7fd5c0e3dfa39469833635fcf9587f6beacefac2
yadavdeepali <95540624+yadavdeepali@users.noreply.github.com>
2021-12-04 22:16:45 +0530

    Create ques3.py

---
+++ ques3.py
@@ -0,0 +1,13 @@
+user1= int(input("enter the number "))
+user2= int(input("enter the number"))
+user3 = int (input("enter the number"))
+
+    
+if user1 <=user2>user3 or user1 > user2>=user3 or user3>=user2>user2 or user3>user2>user1 :
+    print("user 1")
+elif user2>=user1 or user2>user3>=user1 or user1>=user3>user2 or  user1 >= user3>user2:
+     print("user2")
+elif user3>=user1>user2 or user3 > user1>=user2 or user3>user1>=user3:
+    print("user 3")
+else:
+    print("nothing") 
