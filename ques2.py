bebbbd4385cd8cc0782f9e64d3950f5fe7586c13
yadavdeepali <95540624+yadavdeepali@users.noreply.github.com>
2021-12-04 22:15:51 +0530

    Create ques2.py

---
+++ ques2.py
@@ -0,0 +1,13 @@
+day =input("enter the day")
+mealtime =input ("enter the menu time")
+if day== "monday":
+    if mealtime=="breakfast":
+        print("pooha")
+        if mealtime=="lunch":
+            print("subji ,chappti ,rice")
+            if mealtime=="scanks":
+                print("bhel")
+                if mealtime=="dinner":
+                    print("rice,chappti, sabji")
+else :
+    print ("nothing")                    
